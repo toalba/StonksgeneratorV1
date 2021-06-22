1cc180e77571529b964498fb24d20f8940d5474c
Toalba <tb@kleinundpartner.at>
2021-06-22 13:42:42 +0200

    Db begin

--- src/Stonksgenerator/Simulation.java
+++ src/Stonksgenerator/Simulation.java
@@ -14,10 +14,22 @@ public class Simulation {
     private static String userName = "root";
     private static String password = "wa22er!wasser";
 
+    public static LocalDate startdate;
+    public static LocalDate enddate;
+
     private static ArrayList<Closevalue> close_;
     private static ArrayList<Avg> avg_;
     private double bank;
     private double depot;
+    public ArrayList<Totalcount> totalcount;
+
+    private void getWerte_(String symbol)
+    {
+        String sql = "select datum,close from " + symbol + " where datum between \'" + startdate + "\' AND \'" + enddate + "\' ;";
+        String sqlAvg = "select AVERAGE from " + symbol + "avg where datum between \'" + startdate + "\' " + "AND \'" + enddate + "\';";
+
+
+    }
 
     public void trading200()
     {
@@ -25,19 +37,26 @@ public class Simulation {
         {
             if (v.close>avg_.stream().filter(x->x.Date==v.Date).findFirst().get().avg)
             {
-                if(v.Date.getDayOfWeek().equals(DayOfWeek.SATURDAY)||v.Date.getDayOfWeek().equals(DayOfWeek.SUNDAY))
+                if(!v.Date.getDayOfWeek().equals(DayOfWeek.SATURDAY) || !v.Date.getDayOfWeek().equals(DayOfWeek.SUNDAY))
                 {
                     double a = Math.floor(bank/v.close);
-
+                    depot = a;
+                    bank = bank-a*v.close;
                 }
             }
             if (v.close<avg_.stream().filter(x->x.Date==v.Date).findFirst().get().avg)
             {
-                if(v.Date.getDayOfWeek().equals(DayOfWeek.SATURDAY)||v.Date.getDayOfWeek().equals(DayOfWeek.SUNDAY))
+                if(!v.Date.getDayOfWeek().equals(DayOfWeek.SATURDAY) || !v.Date.getDayOfWeek().equals(DayOfWeek.SUNDAY))
                 {
-                    //sell
+                    double a = depot*v.close;
+                    bank = a;
+                    depot=0;
                 }
             }
+            Totalcount todaysvalue = new Totalcount();
+            todaysvalue.Date=v.Date;
+            todaysvalue.value=depot*v.close+bank;
+            totalcount.add(todaysvalue);
         }
     }
 }
---
+++ src/Stonksgenerator/Totalcount.java
@@ -0,0 +1,8 @@
+package Stonksgenerator;
+
+import java.time.LocalDate;
+
+public class Totalcount {
+    public LocalDate Date;
+    public double value;
+}
